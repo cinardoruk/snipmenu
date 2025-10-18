1d62cf0ccc31b684ca85e6d36f74e4ba5f5427ee
Cinar Doruk <cinar.doruk@gmail.com>
2025-10-18 15:04:17 +0300

    release: add xclip/dmenu check, refactor a bit into select_snippet(), add .config/snipmenu as the default snippet_dir

--- snipmenu.sh
+++ snipmenu.sh
@@ -1,9 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 :<<COMMENT
 Optional description of script.
 TODO
+* v2.0: snippet previews, center dmenu on the screen, notify that a CRUD operation has completed
 * v1.0: dir has files, we store k:v pairs in filename:content
-* notify that a CRUD operation has completed
+* check necessary programs exist(xclip, dmenu, x11, etc)
+* make repo, push v1.0 to it
+* license it
+* make readme
+* DONE create a systemwide ~/.config/snipmenu dir if it doesn't exist
 * CRUD
 	create
 		* DONE open $EDITOR to add new snippet
@@ -12,29 +17,50 @@ TODO
 	update
 		* DONE open $EDITOR to edit selected snippet file
 	delete
-		* delete selected
+		* DONE delete selected
 
 COMMENT
 
-SNIPPET_DIR="./snippets"
+# Check that dmenu and xclip are installed
+command -v dmenu >/dev/null 2>&1 || { echo "Error: dmenu is required but not installed" >&2; exit 1; }
+command -v xclip >/dev/null 2>&1 || { echo "Error: xclip is required but not installed" >&2; exit 1; }
 
+# set snippet dir and create it if it doesn't exist.
+SNIPPET_DIR="${HOME}/.config/snipmenu/snippets"
+
+[ ! -d "$SNIPPET_DIR" ] && mkdir -p "$SNIPPET_DIR"
+
+# helper
+select_snippet(){
+	ls "$SNIPPET_DIR" | dmenu -i -p "$1"
+}
+
+# CRUD
 create_snippet(){
-	snippet_file_name=$(echo "" | dmenu -p "Enter new snippet name:")
-	$EDITOR "$SNIPPET_DIR"/"$snippet_file_name"
+	local snippet=$(echo "" | dmenu -p "new snippet name:")
+	[ -z "$snippet" ] && return
+	"$EDITOR" "$SNIPPET_DIR"/"$snippet"
 }
 
 read_snippet(){
-	cat $SNIPPET_DIR/$(ls $SNIPPET_DIR | dmenu -i) | xclip -selection clipboard
+	local snippet=$(select_snippet 'copy to clipboard')
+	[ -z "$snippet" ] && return
+	xclip -selection clipboard "$SNIPPET_DIR"/"$snippet"
 }
 
 update_snippet(){
-	$EDITOR $SNIPPET_DIR/$(ls $SNIPPET_DIR | dmenu -i)
+	local snippet=$(select_snippet 'update')
+	[ -z "$snippet" ] && return
+	"$EDITOR" "$SNIPPET_DIR"/"$snippet"
 }
 
 delete_snippet(){
-	rm -f $SNIPPET_DIR/$(ls $SNIPPET_DIR | dmenu -i)
+	local snippet=$(select_snippet 'delete')
+	[ -z "$snippet" ] && return
+	rm -f "$SNIPPET_DIR"/"$snippet"
 }
 
+# main()
 case $1 in
 	create)
 		create_snippet
@@ -47,5 +73,9 @@ case $1 in
 		;;
 	delete)
 		delete_snippet
+		;;
+	*)
+		echo "Usage: $0 {create|read|update|delete}"
+		exit 1
 		;;
 esac
